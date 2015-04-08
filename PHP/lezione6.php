673655736eef6fc33c9d6eab0a2dce692e36732d
sary <sary@sary-K52JU>
2015-04-08 13:28:29 +0200

    Php - lezione 6 (prima pagina php)

---
+++ PHP/lezione6.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Prova Php</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+            echo "Hello Php";
+            echo "<h1>Titolo h1</h1>";
+            
+            $cond = 0;
+            if($cond==0)
+            {
+                echo "<p>Cond uguale a 0</p>";
+            }
+            else
+            {
+                echo "<p>Cond diverso da 0</p>";
+            }
+            
+        ?>
+        <form>
+            <select>
+            <!-- Possono essere aggiunte un numero arbitrario di porzioni di
+                 codice php nel file. Queste vengono utizzate per le sole parti 
+                 che devono essere generate dinamicamente nel documento: 
+                 (nell'esempio la stampa della variabile $i 10 volte) -->
+                 
+            <?php
+                for($i=0; $i<10; $i++)
+                {
+                    ?>
+                <option value="<?php echo $i;?>">
+                    Valore - <?= $i ?> <!-- Sintassi alternativa per la stampa di $i -->
+                </option>
+            <?php
+                }
+            ?>
+            </select>
+        </form>
+    </body>
+</html>
