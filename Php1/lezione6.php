3d4867d4fbea14538e265680d27a67358d116640
sary <sary@sary-K52JU>
2015-04-08 13:34:48 +0200

    Php - lezione 6 (Intro php)

--- Php1/lezione6.php
+++ Php1/lezione6.php
@@ -15,25 +15,31 @@ and open the template in the editor.
             echo "Hello Php";
             echo "<h1>Titolo h1</h1>";
             
+            //Stampa
             $cond = 0;
             if($cond==0)
             {
-                echo "<p>Cond uguale a 0</p>";
+                echo "<p>Cond uguale a $cond</p>";
             }
             else
             {
-                echo "<p>Cond diverso da 0</p>";
+                echo "<p>Cond diverso da zero - $cond</p>";
             }
             
         ?>
         <form>
             <select>
+            <!-- Possono essere aggiunte un numero arbitrario di porzioni di
+                 codice php nel file. Queste vengono utizzate per le sole parti 
+                 che devono essere generate dinamicamente nel documento: 
+                 (nell'esempio la stampa della variabile $i 10 volte) -->
+                 
             <?php
                 for($i=0; $i<10; $i++)
                 {
                     ?>
                 <option value="<?php echo $i;?>">
-                    Valore - <?= $i ?>
+                    Valore - <?= $i ?> <!-- Sintassi alternativa per la stampa di $i -->
                 </option>
             <?php
                 }
